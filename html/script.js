a2f02a725a0c575ef0ca799c6a559b49802fcbea
Aritz T <59333567+eltrevii@users.noreply.github.com>
2023-07-17 22:41:57 +0200

    Add files via upload

--- html/script.js
+++ html/script.js
@@ -1,19 +1,42 @@
 $(window).on("load", function(){
+    imgArr = []
+    imgArr[0] = ""
     //$("body").css("background-color", "black")
-    document.getElementById('video').addEventListener('ended',myHandler,false);
+    $('#video')[0].addEventListener('ended',myHandler,false);
     function myHandler(e) {
         $("#intro").fadeOut()
     }
-    var img = new Image();
-        img.src = "https://source.unsplash.com/random/7680x4320";
-        //$("body").css("background-color", "#202020")
-        img.onload = function() {
-            $("body").css("background", "linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(" + img.src + ")")
-            hide()
+        imgArr[1] = new Image();
+        imgArr[2] = new Image();
+        imgArr[3] = new Image();
+        
+        imgArr[1].src = "https://source.unsplash.com/random/7680x4320?img=1";
+        imgArr[1].onload = function() {
+            console.log("1 ready")
+            imgArr[2].src = "https://source.unsplash.com/random/7680x4320?img=2";
+            imgArr[2].onload = function() {
+                console.log("2 ready")
+                imgArr[3].src = "https://source.unsplash.com/random/7680x4320?img=3";
+                imgArr[3].onload = function() {
+                    console.log("3 ready")
+                    img_sel(1)
+                    hide()
+                }
+            }
         }
+    var matches = document.querySelectorAll('.radio1');
+    for (match in matches) {
+        matches[match].onchange = function() {
+        img_sel(this.value)
+       }
+    }
 });
 
 hide = function() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("page").style.display = "block";
+    $("#page")[0].style.display = "block";
+    $("#loader-parent").fadeOut(750);
+}
+
+img_sel = function(img) {
+    $("body").css("background", "linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(" + imgArr[img].src + ")")
 }
