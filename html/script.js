b0f6098a05e0cc97a0eeaee91563fbb76eec4f79
Aritz T <59333567+eltrevii@users.noreply.github.com>
2023-07-17 18:38:13 +0200

    Add files via upload

--- html/script.js
+++ html/script.js
@@ -1,9 +1,15 @@
 $(window).on("load", function(){
-    var img = new Image();
-    img.src = "https://source.unsplash.com/random/3840x2160";
-    img.onload = function() {
-        $("body").css("background", "linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(" + img.src + ")")
-        hide()
+    //$("body").css("background-color", "black")
+    document.getElementById('video').addEventListener('ended',myHandler,false);
+    function myHandler(e) {
+        var img = new Image();
+        img.src = "https://source.unsplash.com/random/7680x4320";
+        $("#intro").fadeOut(400, "linear")
+        //$("body").css("background-color", "#202020")
+        img.onload = function() {
+            $("body").css("background", "linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)), url(" + img.src + ")")
+            hide()
+        }
     }
 });
 
