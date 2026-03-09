4534b3eb6142241b0756b8269b732d97f523c379
trevi <aritz331@gmail.com>
2026-03-09 10:43:42 +0100

    change spaces tot tabs and very slightly organize coode

--- py/translater.py
+++ py/translater.py
@@ -4,59 +4,51 @@ def clear():
     os.system("cls") if os.name == "nt" else os.system("clear")
 
 def main():
-	nttext = []
-	tftext = ""
+    nttext = []
+    tftext = ""
 
-	clear()
+    clear()
 
-	ogstr = input("texto en el lenguaje original (ingles): ")
-	nwstr = ogstr.split(" ")
+    ogstr = input("texto en el lenguaje original (ingles): ")
+    nwstr = ogstr.split(" ")
 
-	print("\ntraduciendo...")
+    print("\ntraduciendo...")
+    print("")
 
-	try:
-		print("")
-		
-		ntext = []
+    ntext = []
 
+    for i in nwstr:
+        if i == "want"  : i = "i want"
+        if i == "wanted": i = "i wanted"
+        ntext.append(i)
 
-		for i in nwstr:
-			if i == "want"  : i = "i want"
-			if i == "wanted": i = "i wanted"
-			ntext.append(i)
+    p_max = len(ntext)
+    p_all = [i for i in range(1, p_max + 1)]
 
-		p_max = len(ntext)
-		p_all = [i for i in range(1, p_max + 1)]
-		
-		pstr  = "[{0:>%d}/{1:>%d}] {2:<%d} =" % (len(str(p_max)), len(str(p_max)), len(max(ntext, key=len)))
-		plist = []
+    pstr  = "[{0:>%d}/{1:>%d}] {2:<%d} =" % (len(str(p_max)), len(str(p_max)), len(max(ntext, key=len)))
+    plist = []
 
-		for i in range(len(p_all)):
-			plist.append(pstr.format(p_all[i], p_max, ntext[i]))
+    for i in range(len(p_all)):
+        plist.append(pstr.format(p_all[i], p_max, ntext[i]))
 
-		for idx, i in enumerate(ntext):
-			print(f"{plist[idx]}", end="")
-			r = re.post(f"https://libretranslate.org/translate?q={i}&source=en&target=es&format=text&api_key=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx").text
-			
-			trtext = js.loads(r)['translatedText']
-			if trtext == "i": trtext = "yo"
-			print(f" {trtext}")
-			nttext.append(trtext)
+    for idx, i in enumerate(ntext):
+        print(f"{plist[idx]}", end="")
+        r = re.post(f"https://libretranslate.org/translate?q={i}&source=en&target=es&format=text&api_key=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx").text
 
-	except Exception as e:
-		print(f"\n\nerror: {e}")
-		return 1
+        trtext = js.loads(r)['translatedText']
+        if trtext == "i": trtext = "yo"
+        print(f" {trtext}")
+        nttext.append(trtext)
 
-	
-	otstr = re.post(f"https://libretranslate.org/translate?q={ogstr}&source=en&target=es&format=text&api_key=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx").text
+    otstr = re.post(f"https://libretranslate.org/translate?q={ogstr}&source=en&target=es&format=text&api_key=xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx").text
 
-	for i in nttext:
-		tftext += i + " "
+    for i in nttext:
+        tftext += i + " "
 
-	print("")
-	print(f"texto traducido: {tftext}")
-	print(f"texto original : {js.loads(otstr)['translatedText']}")
-	print("\nadvertencia: el texto original traducido podria no ser 100% preciso")
+    print("")
+    print(f"texto traducido: {tftext}")
+    print(f"texto original : {js.loads(otstr)['translatedText']}")
+    print("\nadvertencia: el texto original traducido podria no ser 100% preciso")
 
 if __name__ == '__main__':
-	main()
+    main()
